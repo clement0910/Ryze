88ce3c6f10d5c9c0f4e189a689035bcb8f5fb16a
Clément Sapt <csapt2001@gmail.com>
2023-05-07 22:52:21 +0200

    create gem :rocket:

---
+++ lib/Ryze.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Ryze/version"
+
+module Ryze
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Ryze_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ryze do
+  it "has a version number" do
+    expect(Ryze::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
