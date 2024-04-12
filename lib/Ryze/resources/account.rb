b2f9a07f710840ac41f6facb1834e81874178892
Clément Sapt <clementsapt@gmail.com>
2024-04-12 20:53:01 +0200

    add default value to tag_line

--- lib/Ryze/resources/account.rb
+++ lib/Ryze/resources/account.rb
@@ -8,9 +8,9 @@ module Ryze
     # Retrieve an account by Riot ID.
     #
     # @param game_name [String] Game name of the account.
-    # @param tag_line [String] Tag line of the account.
+    # @param tag_line [String] Tag line of the account. Default is 'EUW'.
     # @return [Account] Account object.
-    def retrieve_account_by_riot_id(game_name:, tag_line:)
+    def retrieve_account_by_riot_id(game_name:, tag_line: 'EUW')
       Account.new get_request("#{BASE_URL}/by-riot-id/#{game_name}/#{tag_line}").body
     end
   end
